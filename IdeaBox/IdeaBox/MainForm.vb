9211bc62798da7ef0ea42b679254640311f539c3
lyj_86@qq.com <lyj_86@qq.com>
2016-01-28 12:57:41 +0800

    新建项目

---
+++ IdeaBox/IdeaBox/MainForm.vb
@@ -0,0 +1,9 @@
+﻿Imports DevExpress.XtraEditors
+
+
+Partial Public Class MainForm
+    Inherits XtraForm
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+End Class
---
+++ Pickup/Pickup/Program.vb
@@ -0,0 +1,17 @@
+﻿Imports DevExpress.LookAndFeel
+
+Friend NotInheritable Class Program
+    ''' <summary>
+    ''' The main entry point for the application.
+    ''' </summary>
+    Private Sub New()
+    End Sub
+    <STAThread>
+    Shared Sub Main()
+        DevExpress.Skins.SkinManager.EnableFormSkins()
+        UserLookAndFeel.Default.SetSkinStyle("DevExpress Dark Style")
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New MainForm())
+    End Sub
+End Class
