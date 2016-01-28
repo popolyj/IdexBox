5667fc0b68997762e2762559709c0579e76fdc22
popolyj <lyj_86@qq.com>
2016-01-28 16:35:46 +0800

    测试

--- IdeaBox/IdeaBox/MainForm.Designer.vb
+++ IdeaBox/IdeaBox/MainForm.Designer.vb
@@ -26,6 +26,7 @@ Partial Public Class MainForm
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -55,9 +56,19 @@ Partial Public Class MainForm
         Me.Button3.Text = "Button1"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(114, 208)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "Button1"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
         Me.ClientSize = New System.Drawing.Size(1272, 521)
+        Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -71,6 +82,7 @@ Partial Public Class MainForm
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
 
 #End Region
 
